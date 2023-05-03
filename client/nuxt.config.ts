5e24806b8a5c34adba6542c8037e5db990126402
devkya <kihaok@gmail.com>
2023-05-03 20:08:31 +0900

    Update

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -5,24 +5,15 @@ import vuetify from 'vite-plugin-vuetify'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 // @ts-ignore
 export default defineNuxtConfig({
-    css: ['vuetify/styles'], // vuetify ships precompiled css, no need to import sass
-    vite: {
-        // @ts-ignore
-        // curently this will lead to a type error, but hopefully will be fixed soon #justBetaThings
-        ssr: {
-            noExternal: ['vuetify'], // add the vuetify vite plugin
-        },
+    css: ['vuetify/lib/styles/main.sass', '/assets/scss/style.scss'], // vuetify ships precompiled css, no need to import sass
+    build: {
+      transpile: ['vuetify'],
     },
-    modules: [
-        // @ts-ignore
-        // this adds the vuetify vite plugin
-        // also produces type errors in the current beta release
-        async (options, nuxt) => {
-            nuxt.hooks.hook('vite:extendConfig', config => config.plugins.push(
-                vuetify()
-            ))
-        }
-    ],
+    vite: {
+      define: {
+        'process.env.DEBUG': false,
+      },
+    },
   
     target: 'static',
   
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,63 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const chartOptions1 = computed(() => {
+  return {
+    chart: {
+      type: "pie",
+      height: 300,
+      fontFamily: `inherit`,
+      foreColor: "#a1aab2",
+      sparkline: {
+        enabled: true,
+      },
+    },
+    labels: ["extremely Satisfied", "Satisfied", "Poor", "Very Poor"],
+    legend: {
+      show: true,
+      position: "bottom",
+      fontFamily: "inherit",
+      labels: {
+        colors: "inherit",
+      },
+    },
+    dataLabels: {
+      enabled: true,
+      dropShadow: {
+        enabled: false,
+      },
+    },
+    theme: {
+      monochrome: {
+        enabled: true,
+      },
+    },
+  };
+});
+
+// chart 1
+const lineChart1 = {
+  series: [66, 50, 40, 30],
+};
+</script>
 <template>
   <v-container>
     <v-row align="center" justify="center">
-      <v-col cols="12">
-        <h1>Home View</h1>
-      </v-col>
       <v-col>
-        <v-btn color="primary" to="/about">About</v-btn>
+        <v-card elevation="0">
+          <v-card variant="outlined">
+            <v-card-text>
+              <span class="text-subtitle-1">Customer Satisfaction</span>
+              <apexchart
+                type="pie"
+                height="300"
+                :options="chartOptions1"
+                :series="lineChart1.series"
+              >
+              </apexchart>
+            </v-card-text>
+          </v-card>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
